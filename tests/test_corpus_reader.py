f2599bb133ee3fc2dd84617fdaabc146e31f834f
Nico <josenicolasbarrosogarcia@gmail.com>
2026-09-06 18:48:43 +0200

    test: create a valid PDF fixture

--- tests/test_corpus_reader.py
+++ tests/test_corpus_reader.py
@@ -8,7 +8,7 @@ def test_dir_reader(tmp_doc_dir, tmp_file):
     new_tmp_directory = tmp_doc_dir
     print("Temporary directory created", new_tmp_directory)
 
-    tmp_dir_path = os.path.join(new_tmp_directory, "ingest_text_tmp.text")
+    tmp_dir_path = os.path.join(new_tmp_directory, "ingest_text_tmp.pdf")
     new_tmp_file = tmp_file(tmp_dir_path)
     print("Temporary file created", new_tmp_file)
 
